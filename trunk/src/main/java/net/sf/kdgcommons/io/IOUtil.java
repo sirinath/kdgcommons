69d4afb3fa4ac472ca57badcaa1be6b8ab9a36ad
kdgregory <kdgregory@e368c642-3b58-40b6-89f3-1596afd8f84b>
2012-04-16 23:13:33 +0000

    add IOUtil.copy(), IOUtil.createTempFile(InputStream,String)

--- trunk/src/main/java/net/sf/kdgcommons/io/IOUtil.java
+++ trunk/src/main/java/net/sf/kdgcommons/io/IOUtil.java
@@ -18,8 +18,10 @@ import java.io.BufferedInputStream;
 import java.io.Closeable;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.RandomAccessFile;
 import java.util.zip.GZIPInputStream;
 
@@ -35,6 +37,10 @@ public class IOUtil
      *  that it might throw. This is used in a <code>finally</code> block,
      *  where such exceptions would supercede any thrown from code in the
      *  <code>try</code> block.
+     *  <p>
+     *  This method also exists in Apache Commons IO. It exists here because
+     *  older versions of Commons IO took explicit stream types, rather than
+     *  <code>Closeable</code>.
      *
      *  @param  closable    The object to close. May be null, in which
      *                      case this method does nothing.
@@ -52,6 +58,34 @@ public class IOUtil
         {
             // ignore it
         }
+    }
+
+
+    /**
+     *  Copies the input stream to the output stream. Does not close either.
+     *  Either parameter may be null, in which case this method is a no-op.
+     *  <p>
+     *  This method also exists (with many variants) in Jakarta Commons IO.
+     *  It's here so that this library can be self-contained.
+     *
+     *  @return The number of bytes copied.
+     */
+    public static long copy(InputStream in, OutputStream out)
+    throws IOException
+    {
+        if ((in == null) || (out == null))
+            return 0;
+
+        long total = 0;
+        int read = 0;
+        byte[] data = new byte[8192];   // common disk IO size
+        while ((read = in.read(data)) >= 0)
+        {
+            out.write(data, 0, read);
+            total += read;
+
+        }
+        return total;
     }
 
 
@@ -109,6 +143,10 @@ public class IOUtil
         File file = File.createTempFile(prefix, null);
         file.deleteOnExit();
 
+        // there's no need to do anything else for an empty file
+        if (size == 0L)
+            return file;
+
         RandomAccessFile raf = null;
         try
         {
@@ -118,6 +156,29 @@ public class IOUtil
         finally
         {
             closeQuietly(raf);
+        }
+        return file;
+    }
+
+
+    /**
+     *  Creates a temporary file via {@link #createTempFile}, then copies the
+     *  contents of the passed stream to it. Caller is responsible for closing
+     *  the input stream.
+     */
+    public static File createTempFile(InputStream in, String prefix)
+    throws IOException
+    {
+        File file = createTempFile(prefix, 0);
+        FileOutputStream fos = null;
+        try
+        {
+            fos = new FileOutputStream(file);
+            copy(in, fos);
+        }
+        finally
+        {
+            closeQuietly(fos);
         }
         return file;
     }
