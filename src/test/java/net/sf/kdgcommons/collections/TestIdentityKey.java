3a47c27370ff70b5a0751359e8f762fe933dca7f
kdgregory <kdgregory@e368c642-3b58-40b6-89f3-1596afd8f84b>
2013-06-20 00:31:47 +0000

    IdentityKey: repackage; deprecate old version

--- src/test/java/net/sf/kdgcommons/collections/TestIdentityKey.java
+++ src/test/java/net/sf/kdgcommons/collections/TestIdentityKey.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package net.sf.kdgcommons.util;
+package net.sf.kdgcommons.collections;
 
 import java.util.HashSet;
 
